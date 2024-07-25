d8a4867b9fa66e38388bf2141ca6f5e97b063cf0
VandaLeng <Vanda.Leng@student.passerellesnumeriques.org>
2024-07-25 12:55:17 +0700

    Add files via upload

---
+++ chepter/Loops/exl.py
@@ -0,0 +1,35 @@
+d = 0
+while d < 31:
+  print(d)
+  d += 1
+  
+i = 0
+sum = 0
+while i <= 15:
+      sum += i
+      i += 1
+print(sum)
+
+
+
+i = 0
+while i <= 15:
+    if i % 2 == 0:
+        print(i)
+    i += 1
+
+      
+
+i = 0
+sum = 0 
+while i <= 15:
+    if i % 2 == 1:
+        sum += i
+    i += 1
+print(sum)
+
+  
+i = 0
+while i <= 30:
+      print(30-i)
+      i += 1
